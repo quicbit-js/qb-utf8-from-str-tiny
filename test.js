2bea9ab91850ccaae2e10457a15a2f923083b63d
mvoss9000 <mvoss9000@yahoo.com>
2017-03-21 00:17:30 -0400

    update license and package.files.  remove test dependency.

--- test.js
+++ test.js
@@ -11,28 +11,17 @@ test('utf8', function(t) {
             [ 'abc\uD801\uDC00',     [0x61,0x62,0x63,0xF0,0x90,0x90,0x80]   ],
             [ '在嚴寒的冬日裡',        [229,156,168,229,154,180,229,175,146,231,154,132,229,134,172,230,151,165,232,163,161] ],
             [ '"abc"%',              [ 34,97,98,99,34,37 ] ],
+            [ 'abc𐐀defg,é',          [ 97, 98, 99, 240, 144, 144, 128, 100, 101, 102, 103, 44, 195, 169 ]       ],
+            [ 'gîddñup𐂃!',          [ 103, 195, 174, 100, 100, 195, 177, 117, 112, 240, 144, 130, 131, 33 ]     ],
+            [ 'ᄒ,ᅡ,ᆫ,한',                           [ 225, 132, 146, 44, 225, 133, 161, 44, 225, 134, 171, 44, 237, 149, 156 ]    ],
         ],
         utf8_from_str
     )
 })
 
-test('utf8 and utf8_to_str in harmony and at peace with the world', function(t) {
-    t.tableAssert(
-        [
-            [ 'v',                                  'exp'               ],
-            [ '',                                   ''                  ],
-            [ '在嚴寒的冬日裡',                       '在嚴寒的冬日裡'      ],
-            [ 'abc𐐀defg,é',                         'abc𐐀defg,é'        ],
-            [ 'gîddñup𐂃!',                         'gîddñup𐂃!'       ],
-            [ 'ᄒ,ᅡ,ᆫ,한',                           'ᄒ,ᅡ,ᆫ,한'        ],
-        ],
-        function(v) { return utf8_to_str(utf8_from_str(v)) }
-    )
-})
-
 test('utf8 and utf8_to_str - all ascii', function(t) {
-    for(var i=1; i<128; i+=11) {
-        t.same(utf8_from_str(utf8_to_str([i])), [i], t.desc('ascii', [i], i))
+    for(var i=0; i<128; i++) {
+        t.same(utf8_from_str(String.fromCharCode(i)), [i], t.desc('ascii', [i], i))
     }
     t.end()
 })
